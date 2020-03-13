4d8c95f320ec6d038913ba631d6ea23224375b4a
Neba Funwi-gabga <neba.funwi-gabga@un.org>
2020-03-13 20:23:55 +0100

    Added href tags

--- layouts/partials/navigation.vue
+++ layouts/partials/navigation.vue
@@ -1,12 +1,7 @@
 <template>
   <header class="fixed-topx">
     <nav class="navbar navbar-expand-lg navbar-dark align-items-center">
-      <nuxt-link
-        :to="{ name: 'index' }"
-        class="navbar-brand text-uppercase fw-500"
-      >
-        DesignHouse
-      </nuxt-link>
+      <a href="/" class="navbar-brand text-uppercase fw-500">DesignHouse</a>
       <button
         class="navbar-toggler mr-auto"
         type="button"
@@ -21,16 +16,14 @@
       <div class="collapse navbar-collapse" id="navbar">
         <ul class="navbar-nav font-14 fw-300">
           <li class="nav-item">
-            <nuxt-link :to="{ name: 'designs.search' }" class="nav-link"
-              >Designs</nuxt-link
-            >
+            <a href="/designs" class="nav-link">Designs</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#" title="Designers"> Designers </a>
+            <a class="nav-link" href="#" title="Designers">Designers</a>
           </li>
         </ul>
         <div class="header-search">
-          <form action="" method="">
+          <form action method>
             <div class="form-group">
               <input
                 type="text"
@@ -45,9 +38,7 @@
             </div>
           </form>
         </div>
-        <div
-          class="upload-shot white-path font-14 fw-500 text-uppercase mr-auto"
-        >
+        <div class="upload-shot white-path font-14 fw-500 text-uppercase mr-auto">
           <a href="/upload" class="primary-bg-color text-white">
             <i class="fas fa-cloud-upload-alt"></i> Upload
           </a>
@@ -58,10 +49,10 @@
       <template v-if="!$auth.loggedIn">
         <ul class="before-login font-14 fw-300 text-uppercase">
           <li>
-            <nuxt-link :to="{ name: 'register' }">Sign Up</nuxt-link>
+            <a href="/register">Sign Up</a>
           </li>
           <li>
-            <nuxt-link :to="{ name: 'login' }">Login</nuxt-link>
+            <a href="/login">Login</a>
           </li>
         </ul>
       </template>
@@ -83,21 +74,22 @@
             >
               <img class="user-thumb" :src="$auth.user.photo_url" />
               <div class="usr-info">
-                <span class="user-name font-14 fw-500">{{
+                <span class="user-name font-14 fw-500">
+                  {{
                   $auth.user.name
-                }}</span>
-                <span class="user-deg font-10 fw-300">{{
+                  }}
+                </span>
+                <span class="user-deg font-10 fw-300">
+                  {{
                   $auth.user.tagline
-                }}</span>
+                  }}
+                </span>
                 <span class="down-chevron">
                   <i class="fa fa-angle-down"></i>
                 </span>
               </div>
             </a>
-            <div
-              class="dropdown-menu user-dropdown font-14 fw-500"
-              aria-labelledby="userDropdown"
-            >
+            <div class="dropdown-menu user-dropdown font-14 fw-500" aria-labelledby="userDropdown">
               <div class="dropdown-title-group font-12 fw-500">
                 <span class="dropdown-title text-uppercase">Your Account</span>
               </div>
